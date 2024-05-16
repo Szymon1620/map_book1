7d5e12e3c602c6099dba6547f91b1cf73f70f025
Szymon1620 <166690616+Szymon1620@users.noreply.github.com>
2024-05-16 08:39:39 +0200

    wip: add update user function

--- notatki.py
+++ notatki.py
@@ -2,11 +2,13 @@ from models.data import users
 from utils.crud import show_users
 
 
-def remove_user(users)-> None:
+def update_user(users)-> None:
     kogo_szukasz=input("Kogo szukasz?:")
     for user in users:
         if user['name']==kogo_szukasz:
-            users.remove(user)
-
-search_user(users)
+            user['name']=input("Podaj nowe imię:")
+            user['surname']=input("Podaj nowe nazwisko:")
+            user['posts']=input("Podaj liczbę postów:")
+print(users)
+update_user(users)
 print(users)
--- utils/crud.py
+++ utils/crud.py
@@ -20,3 +20,14 @@ def remove_user(users) -> None:
         if user['name'] == kogo_szukasz:
             users.remove(user)
 
+
+
+
+def update_user(users)-> None:
+    kogo_szukasz=input("Kogo szukasz?:")
+    for user in users:
+        if user['name']==kogo_szukasz:
+            user['name']=input("Podaj nowe imię:")
+            user['surname']=input("Podaj nowe nazwisko:")
+            user['posts']=input("Podaj liczbę postów:")
+
